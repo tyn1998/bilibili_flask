cdba9bcc3fc7fe40e97520a4902dc39c901d9f1b
tyn <1589972754@qq.com>
2020-02-09 19:01:02 +0800

    虽然还没到那一步，先把跨域解决了。

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import *
 
 app = Flask(__name__)
 
 # 防止jsonify自动按字母表顺序排序
 app.config['JSON_SORT_KEYS'] = False
+
+# 解决跨域
+CORS(app, supports_credentials=True)
 
 from .api import api as api_blueprint
 
