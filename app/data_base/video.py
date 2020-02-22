2b9fdf079a61bb8d75ca62d7e44d148bd54f5049
tyn <1589972754@qq.com>
2020-02-22 13:15:10 +0800

    【data_base/video.py】重构，将with改成DB类中的execute方法。

--- app/data_base/video.py
+++ app/data_base/video.py
@@ -11,17 +11,11 @@ def delete(av):
     sql4 = 'delete from v_subreplies where av = %s'
     sql5 = 'delete from v_danmus where av = %s'
     params = (av,)
-    with db as cur:
-        try:
-            cur.execute(sql1, params)
-            cur.execute(sql2, params)
-            cur.execute(sql3, params)
-            cur.execute(sql4, params)
-            cur.execute(sql5, params)
-            db.conn.commit()
-        except Exception as e:
-            print(e)
-            db.conn.rollback()
+    db.execute(sql1, params)
+    db.execute(sql2, params)
+    db.execute(sql3, params)
+    db.execute(sql4, params)
+    db.execute(sql5, params)
 
 
 def write(av):
@@ -43,13 +37,7 @@ def write(av):
         v_info['reply_count'],
         v_info['description']
     )
-    with db as cur:
-        try:
-            cur.execute(sql, params)
-            db.conn.commit()
-        except Exception as e:
-            print(e)
-            db.conn.rollback()
+    db.execute(sql, params)
 
     for tag in v_info['tags']:
         sql = 'insert into v_tags values(%s,%s,%s)'
@@ -58,13 +46,7 @@ def write(av):
             tag['tag_id'],
             tag['tag_name']
         )
-        with db as cur:
-            try:
-                cur.execute(sql, params)
-                db.conn.commit()
-            except Exception as e:
-                print(e)
-                db.conn.rollback()
+        db.execute(sql, params)
 
     for reply in v_info['replies']:
         sql = 'insert into v_replies values(%s,%s,%s,%s,%s,%s,%s,%s)'
@@ -78,14 +60,7 @@ def write(av):
             reply['sex'],
             reply['like'],
         )
-        with db as cur:
-            try:
-                cur.execute(sql, params)
-                db.conn.commit()
-            except Exception as e:
-                print(e)
-                db.conn.rollback()
-
+        db.execute(sql, params)
         if len(reply['sub_replies']) != 0:
             for sub_reply in reply['sub_replies']:
                 sql = 'insert into v_subreplies values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
@@ -102,13 +77,7 @@ def write(av):
                     sub_reply['sex'],
                     sub_reply['like'],
                 )
-                with db as cur:
-                    try:
-                        cur.execute(sql, params)
-                        db.conn.commit()
-                    except Exception as e:
-                        print(e)
-                        db.conn.rollback()
+                db.execute(sql, params)
         else:
             pass
 
@@ -121,13 +90,7 @@ def write(av):
             danmu['coded_uid'],
             danmu['content'],
         )
-        with db as cur:
-            try:
-                cur.execute(sql, params)
-                db.conn.commit()
-            except Exception as e:
-                print(e)
-                db.conn.rollback()
+        db.execute(sql, params)
 
 
 def read(av):
@@ -135,101 +98,94 @@ def read(av):
 
     sql = 'select * from v_basic where av = %s'
     params = (av,)
-    with db as cur:
-        try:
-            cur.execute(sql, params)
-            result = cur.fetchone()
-            info['av'] = result['av']
-            info['title'] = result['title']
-            info['view'] = result['view_count']
-            info['like'] = result['like_count']
-            info['favorite'] = result['favorite_count']
-            info['coin'] = result['coin_count']
-            info['share'] = result['share_count']
-            info['danmu_count'] = result['danmu_count']
-            info['reply_count'] = result['reply_count']
-            info['description'] = result['des']
-        except Exception as e:
-            print(e)
+    try:
+        result = db.execute(sql, params)[0]
+        info['av'] = result['av']
+        info['title'] = result['title']
+        info['view'] = result['view_count']
+        info['like'] = result['like_count']
+        info['favorite'] = result['favorite_count']
+        info['coin'] = result['coin_count']
+        info['share'] = result['share_count']
+        info['danmu_count'] = result['danmu_count']
+        info['reply_count'] = result['reply_count']
+        info['description'] = result['des']
+    except Exception as e:
+        print(e)
 
     tags = []
     sql = 'select * from v_tags where av = %s'
     params = (av,)
-    with db as cur:
-        try:
-            cur.execute(sql, params)
-            for item in cur.fetchall():
-                tag = {
-                    'tag_id': item['tag_id'],
-                    'tag_name': item['tag_name'],
-                }
-                tags.append(tag)
-        except Exception as e:
-            print(e)
+    try:
+        result = db.execute(sql, params)
+        for item in result:
+            tag = {
+                'tag_id': item['tag_id'],
+                'tag_name': item['tag_name'],
+            }
+            tags.append(tag)
+    except Exception as e:
+        print(e)
     info['tags'] = tags
 
     replies = []
     sql = 'select * from v_replies where av = %s'
     params = (av,)
-    with db as cur:
-        try:
-            cur.execute(sql, params)
-            for item in cur.fetchall():
-                sub_replies = []
-                sql = 'select * from v_subreplies where av = %s and root = %s'
-                params = (av, item['rpid'])
-                temp_db = DB(passwd='TYn13646825688', db='bilibili_flask')
-                with temp_db as sub_cur:
-                    try:
-                        sub_cur.execute(sql, params)
-                        for sub_item in sub_cur.fetchall():
-                            sub_reply = {
-                                'rpid': sub_item['rpid'],
-                                'root': sub_item['root'],
-                                'parent': sub_item['parent'],
-                                'dialog': sub_item['dialog'],
-                                'content': sub_item['content'],
-                                'time': sub_item['bj_time'],
-                                'uid': sub_item['uid'],
-                                'umame': sub_item['uname'],
-                                'sex': sub_item['sex'],
-                                'like': sub_item['like_count']
-                            }
-                            sub_replies.append(sub_reply)
-                    except Exception as e:
-                        print(e)
+    try:
+        result = db.execute(sql, params)
+        for item in result:
+            sub_replies = []
+            sql = 'select * from v_subreplies where av = %s and root = %s'
+            params = (av, item['rpid'])
+            try:
+                sub_result = db.execute(sql, params)
+                for sub_item in sub_result:
+                    sub_reply = {
+                        'rpid': sub_item['rpid'],
+                        'root': sub_item['root'],
+                        'parent': sub_item['parent'],
+                        'dialog': sub_item['dialog'],
+                        'content': sub_item['content'],
+                        'time': sub_item['bj_time'],
+                        'uid': sub_item['uid'],
+                        'umame': sub_item['uname'],
+                        'sex': sub_item['sex'],
+                        'like': sub_item['like_count']
+                    }
+                    sub_replies.append(sub_reply)
+            except Exception as e:
+                print(e)
 
-                reply = {
-                    'rpid': item['rpid'],
-                    'content': item['content'],
-                    'time': item['bj_time'],
-                    'uid': item['uid'],
-                    'umame': item['uname'],
-                    'sex': item['sex'],
-                    'like': item['like_count'],
-                    'sub_replies': sub_replies
-                }
-                replies.append(reply)
-        except Exception as e:
-            print(e)
+            reply = {
+                'rpid': item['rpid'],
+                'content': item['content'],
+                'time': item['bj_time'],
+                'uid': item['uid'],
+                'umame': item['uname'],
+                'sex': item['sex'],
+                'like': item['like_count'],
+                'sub_replies': sub_replies
+            }
+            replies.append(reply)
+    except Exception as e:
+        print(e)
     info['replies'] = replies
 
     danmus = []
     sql = 'select * from v_danmus where av = %s'
     params = (av,)
-    with db as cur:
-        try:
-            cur.execute(sql, params)
-            for item in cur.fetchall():
-                danmu = {
-                    'time': item['v_time'],
-                    'beijing_time': item['bj_time'],
-                    'coded_uid': item['coded_uid'],
-                    'content': item['content'],
-                }
-                danmus.append(danmu)
-        except Exception as e:
-            print(e)
+    try:
+        result = db.execute(sql, params)
+        for item in result:
+            danmu = {
+                'time': item['v_time'],
+                'beijing_time': item['bj_time'],
+                'coded_uid': item['coded_uid'],
+                'content': item['content'],
+            }
+            danmus.append(danmu)
+    except Exception as e:
+        print(e)
     info['danmus'] = danmus
 
     return info
